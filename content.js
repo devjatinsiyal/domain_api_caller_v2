cb331f9650db311a3c65cdcd3edf8c2ff3531d1f
Jatin Siyal <jatinsiyal@Jatins-MacBook-Pro.local>
2023-11-02 22:25:53 +0530

    Bhaisaab

---
+++ content.js
@@ -0,0 +1,12 @@
+
+// console.log('HOSTNAME ---->>>>', document.location.hostname)
+
+// let hostname = document.location.hostname
+// let url = 'https://localhost.com:5000/'+hostname
+
+// console.log('OUR URLLLLLL--->>>>>',url)
+
+// fetch(url)
+
+
+fetch('https://localhost.com:5000/'+document.location.hostname)
